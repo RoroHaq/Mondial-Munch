be1375cd0c361acb87009980c52c27698113ee08
Safin Haque <safinhq13@gmail.com>
2024-03-12 22:49:45 -0400

    Added Some sample skeleton Classes

---
+++ MondialMunch/Recipe.cs
@@ -0,0 +1,11 @@
+
+
+public class Recipe{
+
+  /*
+    Will be adding the various fields for the specific recipe needed and an extra property/field called 
+    Country for the dishe's origin
+
+    Other fields like the ingredients, rating, name, serving sizes
+  */
+}
