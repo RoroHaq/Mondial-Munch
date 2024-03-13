73979814807a268cecdf533bbad8ffb3dac14d7f
Safin Haque <safinhq13@gmail.com>
2024-03-12 23:32:56 -0400

    Making methods for Operations

--- MondialMunch/Operations.cs
+++ MondialMunch/Operations.cs
@@ -1,5 +1,43 @@
 
 
 public class Operations{
-  
+
+  //Most of the Methods will inevitably use GUI inputs
+
+  public void NewRecipe(){
+
+    /*
+      Make the users type in the proper inputs to create one
+      Validation will be included
+      Recipe NewRecipe = new Recipe(**inputs)
+
+      return NewRecipe
+    */
+
+  }
+
+  public void OwnerUpdateDescription(String description) {
+
+
+  }
+
+  public void OwnerUpdateTime(int Time) {
+
+  }
+
+  public void OwnerUpdateServings(int servings) {
+    
+  }
+
+  public void OwnerUpdateInstructions(String Instructions) {
+    
+  }
+
+  public void OwnerUpdateIngredients(String Ingredients) {
+    
+  }
+
+  public void OwnerUpdatetags(String tag) {
+
+  }
 }
