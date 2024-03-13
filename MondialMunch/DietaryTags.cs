621375ff8c5ae1af545e0d66c67d3daa7ee451e0
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-03-12 23:50:22 -0400

    Recipe skeleton class completed and dietary tags enum added

---
+++ MondialMunch/DietaryTags.cs
@@ -0,0 +1,9 @@
+public enum DietaryTags{
+    //List of tags
+    //some basic ones below for testing purposes
+    Vegetarian,
+    Vegan,
+    Pescetarian,
+    Kosher,
+    Hallal,
+}
--- MondialMunch/Recipe.cs
+++ MondialMunch/Recipe.cs
@@ -1,11 +1,17 @@
-
-
 public class Recipe{
 
-  /*
-    Will be adding the various fields for the specific recipe needed and an extra property/field called 
-    Country for the dishe's origin
-
-    Other fields like the ingredients, rating, name, serving sizes
-  */
+    public String _name { get;}
+    public User _creator { get;}
+    public String? _description { get; private set;}
+    public int _servings { get;}
+    public double _prep_time { get;}
+    public double _cooking_time { get;}
+    public double total_time => _prep_time + _cooking_time;
+    public Country? _country { get;}
+    public String[] _insturctions { get; private set;}
+    public String[] _regular_ingredients { get; private set;}
+    public String[] _ingredients_amounts { get; private set;}
+    public String[] _substitutions { get; private set;}
+    public DietaryTags tags { get; private set;}
+    public int _stars { get; private set;}
 }
