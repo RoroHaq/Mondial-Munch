e33439cdbc80c99599730aac06e813a243f58713
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-03-12 23:30:15 -0400

    Added User skeleton class

--- MondialMunch/User.cs
+++ MondialMunch/User.cs
@@ -1,14 +1,28 @@
-
-
 public class Users{
 
-  /*
-    This will have the properties/fields:
-    -name
-    -pfp
-    -description
-    -password
-    -A List of Recipes the Individual made
-    -Recipes they are interested in
-  */
+    private String _name { get; private set;}
+    private String? _pfp_path { get; private set;}
+    private String? _description { get; private set;}
+    private Country _country_origin { get; private set;}
+    private Country? _country_current { get; private set;}
+    private byte[] _password { get; private set;}
+    private Recipe[] _personal_recipes { get; set;}
+    private Recipe[] _favourite_recipies { get; set;}
+    private Recipe[] _todo_recipies { get; set;} 
+    
+    public void ResetPassword(){
+        throw new InvalidOperationException("Method Incomplete!");
+    }
+
+    public void ChangeName(){
+        throw new InvalidOperationException("Method Incomplete!");
+    }
+    
+    public void ChangeDescription(){
+        throw new InvalidOperationException("Method Incomplete!");
+    }
+    
+    public void ChangeCurrentCountry(){
+        throw new InvalidOperationException("Method Incomplete!");
+    }
 }
