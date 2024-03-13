cb6fae8641f5883491f8b01b119b518e78ec8408
Safin Haque <safinhq13@gmail.com>
2024-03-12 23:47:20 -0400

    Added methods for Operations.cs

--- MondialMunch/Operations.cs
+++ MondialMunch/Operations.cs
@@ -16,33 +16,59 @@ public class Operations{
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdateDescription(String description) {
+  public void OwnerUpdateDescription(String description, Recipe recipe) {
 
+    /*
+      It updates the chosen recipes description
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdateTime(int Time) {
+  public void OwnerUpdateTime(int Time, Recipe recipe) {
 
+    /*
+      It updates the chosen recipes Time it takes to finish
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdateServings(int servings) {
+  public void OwnerUpdateServings(int servings, Recipe recipe) {
     
+    /*
+      It updates the chosen recipes Servings
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdateInstructions(String Instructions) {
+  public void OwnerUpdateInstructions(String Instructions, Recipe recipe) {
+
+    /*
+      It updates the chosen recipes Instructions
+    */
+    throw new InvalidOperationException("Method Incomplete!");
+  }
+
+  public void OwnerUpdateIngredients(String Ingredient, Recipe recipe) {
     
+    /*
+      It updates the chosen recipes Ingredients
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdateIngredients(String Ingredients) {
-    
+  public void OwnerUpdatetags(String tag, Recipe recipe) {
+
+    /*
+      It updates the chosen recipes tags
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 
-  public void OwnerUpdatetags(String tag) {
+  public void OwnerUpdateFoodCountry(Enum Country, Recipe recipe) {
 
+    /*
+      It updates the chosen recipes Place of Origin
+    */
     throw new InvalidOperationException("Method Incomplete!");
   }
 }
