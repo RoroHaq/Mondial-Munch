789c61a2ef4d3f23f20ec7cabfb9d61e94aa9a14
Safin Haque <safin.haque@dawsoncollege.qc.ca>
2024-03-27 16:11:04 -0400

    Finished constructor minus validation, also created the Ingredient class

--- MondialMunch/Ingredient.cs
+++ MondialMunch/Ingredient.cs
@@ -1,5 +1,12 @@
 
 
 public class Ingredient{
-    
-}
+    public string Name {get;}
+    public int Quantity {get; internal set;}
+
+    public Ingredient(string name, int quantity){
+        Name= name;
+        Quantity = quantity;
+    }
+
+}   
--- MondialMunch/Recipe.cs
+++ MondialMunch/Recipe.cs
@@ -8,14 +8,31 @@ public class Recipe{
     public double CookingTime { get; internal set;}
     public double TotalTime => PrepTime + CookingTime;
     public Country Country { get; internal set;}
-    public string[] Instructions { get; internal set;}
-    public string[] RegularIngredients { get; internal set;}
-    public string[] IngredientsAmounts { get; internal set;}
+    public List<string> Instructions { get; internal set;}
+    public List<Ingredient> Ingredients { get; internal set;}
     public Dictionary<string, string[]>? Substitutions { get; internal set;}
-    public DietaryTags Tags { get; internal set;}
+    public List<DietaryTags>? Tags { get; internal set;}
     public int Stars { get; internal set;}
 
-    public Recipe(string name, User creator, string description, int servings, double preptime, double cookingTime, Country country, string[] Instructions, ){
+    public Recipe(string name, User creator, string description, int servings, 
+                    double preptime, double cookingTime, Country country, List<string> instructions, List<Ingredient> ingredients){
+        
+        /*
+            PrepTime & Cooking Cant be < 0
+            Description and name cant be "" or " "
+            name can have a size limit but cant be null
+            Description can be null
+            Instructions and Ingredients cannot be a size of 0
+        */
 
+        Name= name;
+        Creator = creator;
+        Description= description;
+        Servings = servings;
+        PrepTime = preptime;
+        CookingTime = cookingTime;
+        Country = country;
+        Instructions = instructions;
+        Ingredients = ingredients;
     }
 }
