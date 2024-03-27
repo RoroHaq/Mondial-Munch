118c6d6b3b5f0911ffdb9c685787cc6124e1562e
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-03-27 16:45:44 -0400

    Began User Constructor (no validation)

--- MondialMunch/User.cs
+++ MondialMunch/User.cs
@@ -1,15 +1,27 @@
 public class User{
 
-    private String _name { get; private set;}
-    private String? _pfp_path { get; private set;}
-    private String? _description { get; private set;}
-    private Country _country_origin { get; private set;}
-    private Country? _country_current { get; private set;}
+    public string Name { get; private set;}
+    public string? ProfilePicturePath { get; private set;}
+    public string? Description { get; private set;}
+    public Country CountryOrigin { get; private set;}
+    public Country? CountryCurrent { get; private set;}
     private byte[] _password { get; private set;}
-    private Recipe[] _personal_recipes { get; set;}
-    private Recipe[] _favourite_recipies { get; set;}
-    private Recipe[] _todo_recipies { get; set;} 
+    public List<Recipe> PersonalRecipes { get; internal set;}
+    public List<Recipe> FavouriteRecipies { get; internal set;}
+    public List<Recipe> TodoRecipies { get; internal set;} 
     
+    public User(string name, string profilePicturePath, string description, Country countryOrigin, Country? countryCurrent, byte[] password){
+        Name = name;
+        ProfilePicturePath = profilePicturePath;
+        Description = description;
+        CountryOrigin = countryOrigin;
+        CountryCurrent = countryCurrent;
+        _password = password;
+        PersonalRecipes = new List<Recipe>();
+        FavouriteRecipies = new List<Recipe>();
+        TodoRecipies = new List<Recipe>();
+    }
+
     public void ResetPassword(){
         throw new InvalidOperationException("Method Incomplete!");
     }
