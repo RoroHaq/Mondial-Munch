30094fa0d447a0885d72b2d6a484d713f4c011de
Safin Haque <safinhq13@gmail.com>
2024-03-29 11:48:49 -0400

    Made the Name Property for Recipe.cs

--- MondialMunch/Recipe.cs
+++ MondialMunch/Recipe.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Recipe{
 
-    public string Name { get; internal set;}
+    private string _name;
     public User Creator { get;}
     public string? Description { get; internal set;}
     public int Servings { get; internal set;}
@@ -24,8 +26,7 @@ public class Recipe{
             Description can be null
             Instructions and Ingredients cannot be a size of 0
         */
-
-        Name= name;
+        _name= name;
         Creator = creator;
         Description= description;
         Servings = servings;
@@ -34,5 +35,19 @@ public class Recipe{
         Country = country;
         Instructions = instructions;
         Ingredients = ingredients;
+    }
+
+    public string Name{
+        get{ return _name;}
+        set{
+            if(string.IsNullOrEmpty(_name)){
+                throw new ArgumentNullException("Name is null or is an Empty String");
+            }
+            else if(_name.Length > 50){
+                throw new ValidationException("Name is too big");
+            }
+
+            _name = value;
+        }
     }
 }
