c03b28be79c9e3e658470a458353c0fe293f6dc3
Safin Haque <safinhq13@gmail.com>
2024-03-29 12:42:32 -0400

    Added Name property for INgredients class

--- MondialMunch/Ingredient.cs
+++ MondialMunch/Ingredient.cs
@@ -1,12 +1,27 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 public class Ingredient{
-    public string Name {get;}
-    public int Quantity {get; internal set;}
+    public string _name;
+    public int _quantity;
 
     public Ingredient(string name, int quantity){
-        Name= name;
-        Quantity = quantity;
+        _name= name;
+        _quantity = quantity;
+    }
+
+    public string Name{
+        get{return _name;}
+
+        internal set{
+            if(string.IsNullOrEmpty(_name) || string.IsNullOrWhiteSpace(_name)){
+                throw new ArgumentNullException("Name is either null, an Empty String or blank");
+            }
+            else if(_name.Length > 50){
+                throw new ValidationException("Name is too big");
+            }
+        }
     }
 
 }   
