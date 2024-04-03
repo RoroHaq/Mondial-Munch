1e90e6aa20a36b4103f824cc4b87c7af251a8b03
Nathan Bokobza <nathan.bokobza@dawsoncollege.qc.ca>
2024-04-03 17:11:38 -0400

    Added Validation to user's Name property and added formatter

--- MondialMunch/User.cs
+++ MondialMunch/User.cs
@@ -1,18 +1,32 @@
 using System.Security.Cryptography;
 using System.Text;
-public class User{
+using System.ComponentModel.DataAnnotations;
+namespace MondialMunch;
+public class User {
+    private string _name;
+    public string Name {
+        get { return _name; }
+        private set {
+            if (string.IsNullOrEmpty(value)) {
+                throw new ArgumentNullException("Name is null or is an Empty String");
+            }
+            else if (value.Length > 50) {
+                throw new ValidationException("Name is too long");
+            }
 
-    public string Name { get; private set;}
-    public string? ProfilePicturePath { get; private set;}
-    public string? Description { get; private set;}
-    public Country CountryOrigin { get; private set;}
-    public Country? CountryCurrent { get; private set;}
-    private byte[] _password { get; set;}
-    public List<Recipe> PersonalRecipes { get; internal set;}
-    public List<Recipe> FavouriteRecipies { get; internal set;}
-    public List<Recipe> TodoRecipies { get; internal set;} 
-    
-    public User(string name, string profilePicturePath, string description, Country countryOrigin, Country? countryCurrent, byte[] password){
+            _name = value;
+        }
+    }
+    public string? ProfilePicturePath { get; private set; }
+    public string? Description { get; private set; }
+    public Country CountryOrigin { get; private set; }
+    public Country? CountryCurrent { get; private set; }
+    private byte[] _password { get; set; }
+    public List<Recipe> PersonalRecipes { get; internal set; }
+    public List<Recipe> FavouriteRecipies { get; internal set; }
+    public List<Recipe> TodoRecipies { get; internal set; }
+
+    public User(string name, string profilePicturePath, string description, Country countryOrigin, Country? countryCurrent, byte[] password) {
         Name = name;
         ProfilePicturePath = profilePicturePath;
         Description = description;
@@ -24,45 +38,49 @@ public class User{
         TodoRecipies = new List<Recipe>();
     }
 
-    public bool ResetPassword(string old_password, string new_password){
-        byte[] old_password_hash = MD5.Create().ComputeHash(ASCIIEncoding.ASCII.GetBytes(old_password));
-        bool same_passwords=true;
-        for(int i = 0; i < old_password_hash.Length; i++){
-            if (old_password_hash[i] != _password[i]){
+    public bool ResetPassword(string old_password, string new_password) {
+        byte[] old_password_hash = MD5.HashData(ASCIIEncoding.ASCII.GetBytes(old_password));
+        bool same_passwords = true;
+        for (int i = 0; i < old_password_hash.Length; i++) {
+            if (old_password_hash[i] != _password[i]) {
                 same_passwords = false;
                 break;
             }
         }
-        if (same_passwords && old_password_hash.Length == _password.Length){
-            _password = MD5.Create().ComputeHash(ASCIIEncoding.ASCII.GetBytes(new_password));
+        if (same_passwords && old_password_hash.Length == _password.Length) {
+            _password = MD5.HashData(ASCIIEncoding.ASCII.GetBytes(new_password));
             return true;
         }
 
         return false;
     }
 
-    public bool ChangeName(string password, string new_name){
-        byte[] password_hash = MD5.Create().ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
-        bool same_passwords=true;
-        for(int i = 0; i < password_hash.Length; i++){
-            if (password_hash[i] != _password[i]){
+    public bool ChangeName(string password, string new_name) {
+        byte[] password_hash = MD5.HashData(ASCIIEncoding.ASCII.GetBytes(password));
+        bool same_passwords = true;
+        for (int i = 0; i < password_hash.Length; i++) {
+            if (password_hash[i] != _password[i]) {
                 same_passwords = false;
                 break;
             }
         }
-        if (same_passwords && password_hash.Length == _password.Length){
-            Name = new_name;
-            return true;
+        if (same_passwords && password_hash.Length == _password.Length) {
+            try {
+                Name = new_name;
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
         }
-
         return false;
     }
-    
-    public void ChangeDescription(){
+
+    public void ChangeDescription() {
         throw new InvalidOperationException("Method Incomplete!");
     }
-    
-    public void ChangeCurrentCountry(){
+
+    public void ChangeCurrentCountry() {
         throw new InvalidOperationException("Method Incomplete!");
     }
 }
