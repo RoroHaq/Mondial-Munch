085002120e68e3ec092988e7c756662ee56db619
andrewrokakis <rokakisandrew@gmail.com>
2024-04-10 10:08:28 -0400

    added view recipe, cleaned up ui

--- MondialMunch/Program.cs
+++ MondialMunch/Program.cs
@@ -3,31 +3,48 @@
 public class Program {
     private static User? currentUser;
 
+    private static void PrintHeading() {
+        Console.WriteLine();
+        Console.WriteLine("#### MONDIAL MUNCH ####");
+        if (currentUser != null) {
+            Console.WriteLine("## Hello, " + currentUser.Name);
+        }
+    }
+
     private static void PromptLogin() {
+        PrintHeading();
+        Console.WriteLine(">> Login");
+
+        // prompt username
         Console.WriteLine("Enter username:");
         string? username = Console.ReadLine();
 
+        // prompt password
         Console.WriteLine("Enter password:");
         string? password = Console.ReadLine();
 
-        if (username == null || password == null) return;
+        // validate login
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) return;
 
         User? user = MockData.Users.FirstOrDefault(u => u.Name == username);
         if (user == null) {
             Console.WriteLine("This user does not exist.");
             return;
         }
-
         if (!user.SamePassword(password)) {
             Console.WriteLine("Wrong password.");
             return;
         }
 
+        // establish login
         currentUser = user;
         Console.WriteLine("Welcome back to Mondial Munch, " + username + "!");
     }
 
     private static void PromptCreateAccount() {
+        PrintHeading();
+        Console.WriteLine(">> Sign up");
+
         // prompt username
         Console.WriteLine("Enter username (leave blank to cancel):");
         string? username = Console.ReadLine();
@@ -84,6 +101,9 @@ public class Program {
     }
 
     private static void PromptUserActionUpdateProfile() {
+        PrintHeading();
+        Console.WriteLine(">> Update profile");
+
         Console.WriteLine("Write your new description (leave blank to skip):");
         string? description = Console.ReadLine();
 
@@ -107,6 +127,10 @@ public class Program {
     }
 
     private static void PromptUserActionUpdatePassword() {
+        PrintHeading();
+        Console.WriteLine(">> Update password");
+
+        // prompt currernt password
         Console.WriteLine("Enter current password:");
         string? currentPassword = Console.ReadLine();
 
@@ -122,12 +146,15 @@ public class Program {
             else break;
         }
 
+        // prompt new password
         Console.WriteLine("Enter new password:");
         string? newPassword = Console.ReadLine();
 
+        // prompt new password confirmation
         Console.WriteLine("Re-enter new password:");
         string? newPassword2 = Console.ReadLine();
 
+        // validate passsword
         if (string.IsNullOrEmpty(newPassword) || string.IsNullOrEmpty(newPassword2)) {
             Console.WriteLine("Null value entered, cancelling operation.");
             return;
@@ -137,6 +164,7 @@ public class Program {
             return;
         }
 
+        // reset password
         if (currentUser.ResetPassword(currentPassword, newPassword) == true) {
             Console.WriteLine("Updated password!");
         }
@@ -146,6 +174,10 @@ public class Program {
     }
 
     private static void PromptUserDeleteAccount() {
+        PrintHeading();
+        Console.WriteLine(">> Delete account");
+
+        // prompt password
         Console.WriteLine("Enter password:");
         string? currentPassword = Console.ReadLine();
 
@@ -161,9 +193,11 @@ public class Program {
             else break;
         }
 
+        // prompt confirmation
         Console.WriteLine("Are you sure you want to delete your account? Type in " + currentUser.Name + " to confirm.");
         string? username = Console.ReadLine();
 
+        // delete account
         if (username != currentUser.Name) {
             Console.WriteLine("Operation cancelled.");
         }
@@ -175,6 +209,8 @@ public class Program {
     }
 
     private static void PromptUserActions() {
+        PrintHeading();
+        Console.WriteLine(">> User actions");
         Console.WriteLine("1. Update profile");
         Console.WriteLine("2. Update password");
         Console.WriteLine("3. Delete account");
@@ -196,6 +232,9 @@ public class Program {
     }
 
     private static void PromptSearchRecipe() {
+        PrintHeading();
+        Console.WriteLine(">> Search recipe");
+
         // input keyword
         Console.WriteLine("Filter by keyword (leave blank to skip):");
         string? keyword = Console.ReadLine();
@@ -258,12 +297,15 @@ public class Program {
         // display
         Console.WriteLine("Found " + recipes.Recipes.Count() + " recipes.");
         foreach (Recipe r in recipes.Recipes) {
-            Console.WriteLine(r.Name + " by " + r.Creator);
+            Console.WriteLine(r.Name + " from " + r.Country.ToString() + " by " + r.Creator.Name + " : " + r.Description);
         }
         Console.WriteLine();
     }
 
     private static void PromptAddRecipe() {
+        PrintHeading();
+        Console.WriteLine(">> Add recipe");
+
         // input name
         Console.WriteLine("Enter recipe name (leave blank to cancel):");
         string? name = Console.ReadLine();
@@ -330,7 +372,46 @@ public class Program {
         Console.WriteLine("Created recipe!");
     }
 
+    public static void PromptViewRecipe() {
+        PrintHeading();
+        Console.WriteLine(">> View recipe");
+
+        // input name
+        Console.WriteLine("Enter recipe name (leave blank to cancel):");
+        string? name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name)) return;
+
+        // get recipe
+        Recipe recipe = MockData.Recipes.FirstOrDefault(r => r.Name == name)!;
+        if (recipe == null) {
+            Console.WriteLine("This recipe does not exist.");
+            return;
+        }
+
+        // print recipe
+        Console.WriteLine("Name: " + recipe.Name);
+        Console.WriteLine("Creator: " + recipe.Creator.Name);
+        Console.WriteLine("Description: " + recipe.Description);
+        Console.WriteLine("Servings: " + recipe.Servings);
+        Console.WriteLine("Prep time: " + recipe.PrepTime + " minutes");
+        Console.WriteLine("Cooking time: " + recipe.CookingTime + " minutes");
+        Console.WriteLine("Country: " + recipe.Country);
+
+        Console.WriteLine("Instructions:");
+        for (int i = 0; i < recipe.Instructions.Count; i++) {
+            Console.WriteLine("\t" + (i + 1) + ". " + recipe.Instructions[i]);
+        }
+
+        Console.WriteLine("Ingredients: ");
+        foreach (Ingredient ingredient in recipe.Ingredients) {
+            Console.WriteLine("\t" + ingredient.Name + ": " + ingredient.Quantity);
+        }
+    }
+
     public static void PromptDeleteRecipe() {
+        PrintHeading();
+        Console.WriteLine(">> Delete recipe");
+
         // prompt enter name
         Console.WriteLine("Enter recipe name to delete (enter blank to cancel):");
         string? name = Console.ReadLine();
@@ -366,6 +447,7 @@ public class Program {
     public static void Main() {
         while (true) {
             if (currentUser == null) {
+                PrintHeading();
                 Console.WriteLine("1. Login");
                 Console.WriteLine("2. Sign up");
 
@@ -382,10 +464,12 @@ public class Program {
                 }
             }
             else {
+                PrintHeading();
                 Console.WriteLine("1. User operations");
                 Console.WriteLine("2. Search for recipes");
-                Console.WriteLine("3. Add recipe");
-                Console.WriteLine("4. Delete recipe");
+                Console.WriteLine("3. View recipe");
+                Console.WriteLine("4. Add recipe");
+                Console.WriteLine("5. Delete recipe");
 
                 string? input = Console.ReadLine();
                 if (input == null) continue;
@@ -398,9 +482,12 @@ public class Program {
                         PromptSearchRecipe();
                         break;
                     case "3":
-                        PromptAddRecipe();
+                        PromptViewRecipe();
                         break;
                     case "4":
+                        PromptAddRecipe();
+                        break;
+                    case "5":
                         PromptDeleteRecipe();
                         break;
                 }
