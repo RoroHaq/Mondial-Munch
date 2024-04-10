4b85f0d47d2fb39ee5b6a3aac21bbb341eb2e93a
andrew rokakis <rokakisandrew@gmail.com>
2024-04-09 22:07:43 -0400

    Basic login and home template implemented

--- MondialMunch/Program.cs
+++ MondialMunch/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MondialMunch;
+
+public class Program {
+    private static User? currentUser;
+
+    private static void PromptLogin() {
+        Console.WriteLine("Enter username:");
+        string? username = Console.ReadLine();
+
+        Console.WriteLine("Enter password:");
+        string? password = Console.ReadLine();
+
+        if (username == null || password == null) return;
+
+        User user = MockData.Users.First(u => u.Name == username);
+        if (user == null) {
+            Console.WriteLine("This user does not exist.");
+            return;
+        }
+
+        if (!user.SamePassword(password)) {
+            Console.WriteLine("Wrong password.");
+            return;
+        }
+
+        currentUser = user;
+    }
+
+    private static void PromptUserActions() {
+        Console.WriteLine("1. Update description");
+        Console.WriteLine("2. Update password");
+        Console.WriteLine("3. Delete account");
+
+        string? input = Console.ReadLine();
+        if (input == null) return;
+    }
+
+    public static void Main() {
+        while (true) {
+            if (currentUser == null) {
+                PromptLogin();
+            }
+            else {
+                Console.WriteLine("1. User operations");
+                Console.WriteLine("2. Search for recipes");
+                Console.WriteLine("3. Add recipe");
+                Console.WriteLine("4. Delete recipe");
+
+                string? input = Console.ReadLine();
+                if (input == null) continue;
+
+                switch (input) {
+                    case "1":
+                        PromptUserActions();
+                        break;
+                }
+            }
+        }
+    }
+}
