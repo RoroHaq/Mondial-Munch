ef01a0e1346bc692bee860d1451472fc2070ab24
NathanBluetornado <87149868+NathanBluetornado@users.noreply.github.com>
2024-04-17 15:17:33 -0400

    Started country class

--- MondialMunch/CountryEnum.cs
+++ MondialMunch/CountryEnum.cs
@@ -1,7 +1,29 @@
-public enum Country{
-    //List of the world's countries
-    // some below for testing purposes
-    Canada,
-    United_States,
-    Mexico
+using System.ComponentModel.DataAnnotations;
+using MondialMunch;
+
+public class Country
+{
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        internal set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException("Country name is either null, has an Empty String or its a white space");
+            }
+
+            _name = value;
+        }
+    }
+    public Country(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
