09fcc39e4ff15274d57e1546a00b53c25afec50f
andrew rokakis <rokakisandrew@gmail.com>
2024-04-17 16:48:21 -0400

    Updated db context to use oracle with env vars

--- MondialMunch/MondialMunchContext.cs
+++ MondialMunch/MondialMunchContext.cs
@@ -5,11 +5,14 @@ using Microsoft.EntityFrameworkCore;
 namespace MondialMunch;
 
 public class MondialMunchContext : DbContext {
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<User> Users { get; set; }
 
+    private readonly string? oracleUser = Environment.GetEnvironmentVariable("MM_ORACLE_USERNAME");
+    private readonly string? oraclePassword = Environment.GetEnvironmentVariable("MM_ORACLE_PASSWORD");
+    private readonly string? oracleSource = Environment.GetEnvironmentVariable("MM_ORACLE_SOURCE");
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-    optionsBuilder.UseOracle(@"User Id=Example;
-                          Password=Example123;
-                          Data source=host:123/pemdas;");
-  }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseOracle($"User Id={oracleUser}; Password={oraclePassword}; Data source={oracleSource};");
+    }
 }
