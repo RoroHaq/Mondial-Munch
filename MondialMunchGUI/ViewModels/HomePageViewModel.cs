7460163d5c730aef5e0ee04168820b9a6cb1073b
Nathan Bokobza <nathan.bokobza@dawsoncollege.qc.ca>
2024-05-07 08:22:49 -0400

    Added Grid menu layout for homepage

--- MondialMunchGUI/ViewModels/HomePageViewModel.cs
+++ MondialMunchGUI/ViewModels/HomePageViewModel.cs
@@ -8,12 +8,22 @@ using System.Reactive.Linq;
 
 
 namespace MondialMunchGUI.ViewModels {
-  public class HomePageViewModel : ViewModelBase {
+    public class HomePageViewModel : ViewModelBase {
 
-    public string greet { get; set; }
+        public string greet { get; set; }
 
-    public HomePageViewModel(User user) {
-      greet = "Welcome to Mondial Munch " + user.Name + "!";
+        public HomePageViewModel(User user) {
+            greet = "Welcome to Mondial Munch " + user.Name + "!";
+
+            ListUsers = new(){
+            new User("Nathan", "w", "d", new Country("Canadia"), new Country("Canadia"), "Hello", User.GenerateSalt()),
+            new User("John", "w", "d", new Country("Canadia"), new Country("Canadia"), "Hello", User.GenerateSalt()),
+            new User("Pauk", "w", "d", new Country("Canadia"), new Country("Canadia"), "Hello", User.GenerateSalt()),
+            new User("Among Us", "w", "d", new Country("Canadia"), new Country("Canadia"), "Hello", User.GenerateSalt()),
+            new User("Cath", "w", "d", new Country("Canadia"), new Country("Canadia"), "Hello", User.GenerateSalt())
+      };
+        }
+
+        public ObservableCollection<User> ListUsers { get; }
     }
-  }
 }
