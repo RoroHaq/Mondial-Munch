cefb4098d38a05a1d23bdeb14bf0125a45f7e044
Safin Haque <safinhq13@gmail.com>
2024-05-19 17:29:37 -0400

    Search bug fixed

---
+++ MondialMunch/Migrations/20240519211828_764076c16b36.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MondialMunch.Migrations
+{
+    /// <inheritdoc />
+    public partial class _764076c16b36 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
