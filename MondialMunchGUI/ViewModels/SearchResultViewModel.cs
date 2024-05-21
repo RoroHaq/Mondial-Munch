29ef33467497134a1870cfa05e61dbbc758b097d
andrewrokakis <rokakisandrew@gmail.com>
2024-05-21 17:55:15 -0400

    Fixed filtering

--- MondialMunchGUI/ViewModels/SearchResultViewModel.cs
+++ MondialMunchGUI/ViewModels/SearchResultViewModel.cs
@@ -10,6 +10,7 @@ namespace MondialMunchGUI.ViewModels {
     public class SearchResultViewModel : ViewModelBase {
         public ReactiveCommand<Recipe, Recipe> ViewRecipe { get; }
 
+        private List<Recipe> _recipes;
         private Country _country;
         private int _minServings;
 
@@ -44,13 +45,18 @@ namespace MondialMunchGUI.ViewModels {
             set => this.RaiseAndSetIfChanged(ref _maxTime, value);
         }
 
+        public List<Recipe> Recipes {
+            get => _recipes;
+            set => this.RaiseAndSetIfChanged(ref _recipes, value);
+        }
+
         public List<Country> Countries { get; }
         public List<User> Users;
 
         public List<string> Usernames = new List<string>();
 
         public List<string> CountryNames = new List<string>();
-        public ReactiveCommand<Unit, IEnumerable<Recipe>> Filter { get; }
+        public ReactiveCommand<Unit, Unit> Filter { get; }
 
         public SearchResultViewModel(IEnumerable<Recipe> recipes) {
 
@@ -66,7 +72,7 @@ namespace MondialMunchGUI.ViewModels {
                 CountryNames.Add(country.Name);
             }
 
-            Recipes = new ObservableCollection<Recipe>(recipes);
+            Recipes = new List<Recipe>(recipes);
 
             ViewRecipe = ReactiveCommand.Create((Recipe recipe) => {
                 return recipe;
@@ -76,26 +82,21 @@ namespace MondialMunchGUI.ViewModels {
                 List<Recipe> RecipesFound = MondialMunchService.GetInstance().GetRecipes();
                 RecipeList FilteredRecipe = new RecipeList(RecipesFound);
 
-                if (MinServing > 0 || MinServing != null) {
+                if (MinServing > 0) {
                     FilteredRecipe.FilterByMinServings(MinServing);
                 }
-                if (MaxServing > 0 || MaxServing != null) {
+                if (MaxServing > 0) {
                     FilteredRecipe.FilterByMaxServings(MaxServing);
                 }
-                if (MinTime > 0 || MinTime != null) {
+                if (MinTime > 0) {
                     FilteredRecipe.FilterByMinTime(MinTime);
                 }
-                if (MaxTime > 0 || MaxTime != null) {
+                if (MaxTime > 0) {
                     FilteredRecipe.FilterByMaxTime(MaxTime);
                 }
-                return recipes;
-            });
 
-            Filter.Subscribe((recipe) => {
-                Recipes = new ObservableCollection<Recipe>(recipe);
+                Recipes = new(FilteredRecipe.Recipes);
             });
         }
-
-        public ObservableCollection<Recipe> Recipes { get; set; }
     }
 }
