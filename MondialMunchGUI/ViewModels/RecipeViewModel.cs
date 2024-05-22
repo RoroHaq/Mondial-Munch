b1724abf97b154af49ddbf5b3e9744465cc769d4
andrewrokakis <rokakisandrew@gmail.com>
2024-05-21 20:29:05 -0400

    Added ratings to RecipeViewModel

--- MondialMunchGUI/ViewModels/RecipeViewModel.cs
+++ MondialMunchGUI/ViewModels/RecipeViewModel.cs
@@ -9,6 +9,12 @@ using ReactiveUI;
 namespace MondialMunchGUI.ViewModels;
 
 public class RecipeViewModel : ViewModelBase {
+    private int _myRating;
+    private string _myReview;
+
+    private double _stars;
+    private ObservableCollection<RecipeReview> _reviews;
+
     public string Name { get; }
     public string Description { get; }
     public string CreatorName { get; }
@@ -18,6 +24,27 @@ public class RecipeViewModel : ViewModelBase {
     public double TotalTime { get; }
     public List<Ingredient> Ingredients { get; }
     public List<RecipeInstruction> Instructions { get; }
+
+    public double Stars {
+        get => _stars;
+        set => this.RaiseAndSetIfChanged(ref _stars, value);
+    }
+    public ObservableCollection<RecipeReview> Reviews {
+        get => _reviews;
+        set => this.RaiseAndSetIfChanged(ref _reviews, value);
+    }
+
+    public int MyRating {
+        get => _myRating;
+        set => this.RaiseAndSetIfChanged(ref _myRating, value);
+    }
+
+    public string MyReview {
+        get => _myReview;
+        set => this.RaiseAndSetIfChanged(ref _myReview, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> AddReview { get; }
 
     public RecipeViewModel(Recipe recipe) {
         Name = recipe.Name;
@@ -29,5 +56,13 @@ public class RecipeViewModel : ViewModelBase {
         TotalTime = recipe.TotalTime;
         Ingredients = recipe.Ingredients;
         Instructions = recipe.Instructions;
+        Stars = recipe.Stars;
+        Reviews = new(recipe.Reviews);
+
+        AddReview = ReactiveCommand.Create(() => {
+            MondialMunchService.GetInstance().AddOrEditRecipeReview(recipe, MyRating, MyReview);
+            Stars = recipe.Stars;
+            Reviews = new(recipe.Reviews);
+        });
     }
 }
